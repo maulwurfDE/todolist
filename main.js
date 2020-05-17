1958eb899ed3d723ef1a9e2567e5c295f3a0b07c
Moritz Hofmann <mowmoh@gmail.com>
2020-05-17 16:35:03 +0200

    Improve priority selector and date selector in modal

--- main.js
+++ main.js
@@ -217,7 +217,7 @@ if (createdProjects.length === 0) {
       name: "Homework",
       description: "English homework",
       dueDate: "01-01-2021",
-      priority: "2",
+      priority: 2,
       itemId: 1,
       project: createdProjects[obj].title.replace(/ /g, "_"),
     });
@@ -225,7 +225,7 @@ if (createdProjects.length === 0) {
       name: "Cleaning",
       description: "Clean my room",
       dueDate: "01-01-2021",
-      priority: "1",
+      priority: 1,
       itemId: 2,
       project: createdProjects[obj].title.replace(/ /g, "_"),
     });
@@ -233,7 +233,7 @@ if (createdProjects.length === 0) {
       name: "Washing dishes",
       description: "Washing dishes from previous day",
       dueDate: "01-01-2021",
-      priority: "1",
+      priority: 1,
       itemId: 3,
       project: createdProjects[obj].title.replace(/ /g, "_"),
     });
@@ -243,7 +243,7 @@ if (createdProjects.length === 0) {
       name: "Help Mum",
       description: "some stuff",
       dueDate: "01-01-2021",
-      priority: "3",
+      priority: 3,
       itemId: 1,
       project: createdProjects[obj].title.replace(/ /g, "_"),
     });
@@ -251,7 +251,7 @@ if (createdProjects.length === 0) {
       name: "Help Grandpa",
       description: "more stuff",
       dueDate: "01-01-2021",
-      priority: "2",
+      priority: 2,
       itemId: 2,
       project: createdProjects[obj].title.replace(/ /g, "_"),
     });
@@ -259,7 +259,7 @@ if (createdProjects.length === 0) {
       name: "Go to work",
       description: "finish my work",
       dueDate: "01-01-2021",
-      priority: "2",
+      priority: 2,
       itemId: 3,
       project: createdProjects[obj].title.replace(/ /g, "_"),
     });
@@ -471,37 +471,28 @@ __webpack_require__.r(__webpack_exports__);
 let activeEditToDo;
 let obj2;
 let itemId;
+let newPriority = 0;
 
 function editButton(edit) {
   document.getElementById(edit).addEventListener("click", function () {
     activeEditToDo = this.id;
-    // this.style.display = "none";
-    console.log(activeEditToDo);
-    let itemId2;
     let str = this.id;
     itemId = str.match(/![\d\D]+$/);
     itemId = JSON.stringify(itemId);
     itemId = itemId.substring(3);
     itemId = itemId.substring(0, itemId.length - 2);
-    console.log(itemId);
 
     let projTitle = str.match(/\/[\d\D]+!/);
     projTitle = JSON.stringify(projTitle);
     projTitle = projTitle.substring(3);
     projTitle = projTitle.substring(0, projTitle.length - 3);
-    console.log(projTitle);
     for (let y = 0; y < _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"].length; y++) {
       if (_projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][y].title.replace(/ /g, "_") === projTitle) {
         obj2 = y;
-        console.log("success " + obj2);
       }
     }
 
-    //   console.log(
-    //    document.getElementById("render").childNodes[itemId].childNodes[2].id
-    //  );
     let modal = document.querySelector(".modal");
-
     modal.style.display = "block";
 
     let closeBtn = document.getElementById("close-btn");
@@ -512,8 +503,20 @@ function editButton(edit) {
           _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].name;
         document.getElementById("editDescription").value =
           _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].description;
-        document.getElementById("editPriority").value =
-          _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].priority;
+        console.log(typeof _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].priority);
+        if (_projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].priority === 1) {
+          newPriority = 1;
+          document.getElementById("button1Modal").classList.remove("blanko");
+        } else if (_projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].priority === 2) {
+          newPriority = 2;
+          document.getElementById("button2Modal").classList.remove("blanko");
+        } else if (_projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].priority === 0) {
+          newPriority = 0;
+          document.getElementById("button0Modal").classList.remove("blanko");
+        } else if (_projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].priority === 3) {
+          newPriority = 3;
+          document.getElementById("button3Modal").classList.remove("blanko");
+        }
         document.getElementById("editDueDate").value =
           _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].dueDate;
         Object(_projectController_js__WEBPACK_IMPORTED_MODULE_0__["updateLocalstorage"])();
@@ -528,20 +531,58 @@ function editButton(edit) {
         modal.style.display = "none";
       }
     };
+
+    document
+      .getElementById("buttonsModal")
+      .addEventListener("click", function (event) {
+        document.getElementById("button0Modal").classList.add("blanko");
+        document.getElementById("button1Modal").classList.add("blanko");
+        document.getElementById("button2Modal").classList.add("blanko");
+        document.getElementById("button3Modal").classList.add("blanko");
+        event.target.classList.remove("blanko");
+        console.log(event.target);
+      });
+
+    document.addEventListener("click", function (event) {
+      console.log(event.target);
+      if (event.target.id === "button2Modal") {
+        newPriority = 2;
+      } else if (event.target.id === "button1Modal") {
+        newPriority = 1;
+      } else if (event.target.id === "button3Modal") {
+        newPriority = 3;
+      } else if (event.target.id === "saveEdit") {
+      } else if (event.target.classList.contains("fa-pen")) {
+      } else if (event.target.id === "editDueDate") {
+      } else {
+        newPriority = 0;
+        document.getElementById("button0Modal").classList.remove("blanko");
+      }
+    });
+    // document
+    //   .getElementById("button1Modal")
+    //   .addEventListener("click", function () {
+    //     newPriority = 1;
+    //   });
+    // document
+    //   .getElementById("button2Modal")
+    //   .addEventListener("click", function () {
+    //     newPriority = 2;
+    //   });
+    // document
+    //   .getElementById("button3Modal")
+    //   .addEventListener("click", function () {
+    //     newPriority = 3;
+    //   });
+    // document
+    //   .getElementById("button0Modal")
+    //   .addEventListener("click", function () {
+    //     newPriority = 0;
+    //   });
   });
 
   let editSave = document.getElementById("saveEdit");
   editSave.addEventListener("click", function () {
-    console.log(this);
-    let str = activeEditToDo;
-    console.log(activeEditToDo);
-    //    let itemId = str.match(/\d+$/);
-
-    //    let str2 = document.getElementById("render").childNodes[itemId].childNodes[2].id;
-
-    // let obj2 = str2.match(/\B\d+/);
-    //   let itemId2 = str2.match(/\d+$/);
-
     for (let e = 0; e < _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist.length; e++) {
       if (_projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].itemId == itemId) {
         _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].name = document.getElementById(
@@ -550,19 +591,20 @@ function editButton(edit) {
         _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].description = document.getElementById(
           "editDescription"
         ).value;
-        _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].priority = document.getElementById(
-          "editPriority"
-        ).value;
+
+        _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].priority = newPriority;
+
         _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][obj2].todolist[e].dueDate = document.getElementById(
           "editDueDate"
         ).value;
       }
     }
+    document.getElementById("button0Modal").classList.add("blanko");
+    document.getElementById("button1Modal").classList.add("blanko");
+    document.getElementById("button2Modal").classList.add("blanko");
+    document.getElementById("button3Modal").classList.add("blanko");
     Object(_projectListViewer_js__WEBPACK_IMPORTED_MODULE_1__["render"])();
     Object(_projectController_js__WEBPACK_IMPORTED_MODULE_0__["updateLocalstorage"])();
-    document.querySelector(".modal").style.display = "none";
-    document.querySelector(".modal").style.display = "none";
-    document.querySelector(".modal").style.display = "none";
     document.querySelector(".modal").style.display = "none";
   });
 }
@@ -594,10 +636,7 @@ __webpack_require__.r(__webpack_exports__);
 
 function render() {
   document.getElementById("render").innerHTML = "";
-  // createDefaultProjectDropdown();
-
   let countListItem = 0;
-
   let activeTab;
   let checkboxTab;
 
@@ -730,60 +769,90 @@ function removeProjectDropdown() {
 
 let selectedPriority = 0;
 
-document.getElementById("button1").addEventListener("click", function () {
-  if (
-    document.getElementById("button1").classList.contains("clicked")
-    //document.getElementById("button1").style.border ===
-    //"5px solid rgba(0, 255, 0, 0.7)"
-  ) {
-    selectedPriority = 0;
-    document.getElementById("button1").style.border = "5px solid white";
-  } else {
-    document.getElementById("button2").style.border = "5px solid white";
-    document.getElementById("button3").style.border = "5px solid white";
-    document.getElementById("button1").style.border =
-      "5px solid rgba(0,255,0,0.7)";
-    selectedPriority = 1;
-  }
-  document.getElementById("button1").classList.toggle("clicked");
-});
+document
+  .getElementById("buttonSelector")
+  .addEventListener("click", function (event) {
+    document.getElementById("button0").classList.add("blanko");
+    document.getElementById("button1").classList.add("blanko");
+    document.getElementById("button2").classList.add("blanko");
+    document.getElementById("button3").classList.add("blanko");
+    event.target.classList.remove("blanko");
+    console.log(event.target);
 
-document.getElementById("button2").addEventListener("click", function () {
-  if (document.getElementById("button2").style.border === "5px solid yellow") {
-    selectedPriority = 0;
-    document.getElementById("button2").style.border = "5px solid white";
-  } else {
-    document.getElementById("button1").style.border = "5px solid white";
-    document.getElementById("button3").style.border = "5px solid white";
-    document.getElementById("button2").style.border = "5px solid yellow";
-    selectedPriority = 2;
-  }
-});
+    if (event.target.id === "button2") {
+      selectedPriority = 2;
+    } else if (event.target.id === "button3") {
+      selectedPriority = 3;
+    } else if (event.target.id === "button1") {
+      selectedPriority = 1;
+    } else {
+      selectedPriority = 0;
+      document.getElementById("button0").classList.remove("blanko");
+    }
+  });
 
-document.getElementById("button3").addEventListener("click", function () {
-  if (
-    document.getElementById("button3").classList.contains("clicked")
-    //document.getElementById("button3").style.border ===
-    //"5px solid rgba(255,0,0,0.7)"
-  ) {
-    selectedPriority = 0;
-    document.getElementById("button3").style.border = "5px solid white";
-  } else {
-    document.getElementById("button1").style.border = "5px solid white";
-    document.getElementById("button2").style.border = "5px solid white";
-    document.getElementById("button3").style.border =
-      "5px solid rgba(255,0,0,0.7)";
-    selectedPriority = 3;
-  }
-  document.getElementById("button3").classList.toggle("clicked");
-});
+// document.getElementById("button1").addEventListener("click", function () {
+//   if (
+//     document.getElementById("button1").classList.contains("clicked")
+//     //document.getElementById("button1").style.backgroundColor===x solid rgba(0, 255, 0, 0.7)"
+//   ) {
+//     selectedPriority = 0;
+//     document.getElementById("button1").style.backgroundColor = "white";
+//   } else {
+//     document.getElementById("button2").style.backgroundColor = "white";
+//     document.getElementById("button3").style.backgroundColor = "white";
+//     document.getElementById("button1").style.backgroundColor =
+//       "rgba(0,255,0,0.7)";
+//     selectedPriority = 1;
+//   }
+//   document.getElementById("button1").classList.toggle("clicked");
+// });
+
+// document.getElementById("button2").addEventListener("click", function () {
+//   if (
+//     document.getElementById("button2").style.backgroundColor ===
+//     "5px solid yellow"
+//   ) {
+//     selectedPriority = 0;
+//     document.getElementById("button2").style.backgroundColor = "white";
+//   } else {
+//     document.getElementById("button1").style.backgroundColor = "white";
+//     document.getElementById("button3").style.backgroundColor = "white";
+//     document.getElementById("button2").style.backgroundColor = "yellow";
+//     selectedPriority = 2;
+//   }
+// });
+
+// document.getElementById("button3").addEventListener("click", function () {
+//   if (
+//     document.getElementById("button3").classList.contains("clicked")
+//     //document.getElementById("button3").style.backgroundColor===x solid rgba(255,0,0,0.7)"
+//   ) {
+//     selectedPriority = 0;
+//     document.getElementById("button3").style.backgroundColor = "white";
+//   } else {
+//     document.getElementById("button1").style.backgroundColor = "white";
+//     document.getElementById("button2").style.backgroundColor = "white";
+//     document.getElementById("button3").style.backgroundColor =
+//       "rgba(255,0,0,0.7)";
+//     selectedPriority = 3;
+//   }
+//   document.getElementById("button3").classList.toggle("clicked");
+// });
 
 document.getElementById("addtodo").addEventListener("click", function () {
   document.getElementById("newTodo").style.display = "block";
+  document.getElementById("button0").classList.remove("blanko");
+  selectedPriority = 0;
 });
 
 document.getElementById("submitAddTodo").addEventListener("click", function () {
   document.getElementById("newTodo").style.display = "none";
+  document.getElementById("button0").classList.add("blanko");
+  document.getElementById("button1").classList.add("blanko");
+  document.getElementById("button2").classList.add("blanko");
+  document.getElementById("button3").classList.add("blanko");
+
   _projectController_js__WEBPACK_IMPORTED_MODULE_0__["createdProjects"][_projectController_js__WEBPACK_IMPORTED_MODULE_0__["obj"]].todolist.unshift(
     new _projectController_js__WEBPACK_IMPORTED_MODULE_0__["ToDo"](
       document.getElementById("title").value,
